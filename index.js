be1132d5a96bb34bff991dd54b1342675c7c8039
Raymond TeHok <tehok.raymond@gmail.com>
2017-08-02 00:33:59 +0200

    Add scheduler

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const script1 = require("./scripts/shortTerm");
-script1.run();
+const script = require("./scripts/shortTerm");
+const schedule = require('node-schedule');
+
+const startTime = new Date();
+startTime.setHours(2, 0, 0, 0);
+
+const rule = new schedule.RecurrenceRule();
+rule.hour = [2, 6, 10, 14, 18, 22];
+rule.minute = 1;
+
+console.log(new Date());
+const s = schedule.scheduleJob({ start: startTime, rule: rule }, function() {
+    script.run();
+});
